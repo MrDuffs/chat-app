e59578562edcf9eef59e321bc2799686214caf9b
MrDuffs <chulkov.i@yandex.ru>
2021-04-27 18:17:03 +0300

    Edit comments to main server file

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
+//Создаем HTTP - сервер
 const server = require('http').createServer()
+
+//Подключаем к серверу Socket.io
+//require('socket.io')(server, ... создает новый экземпляр socket.io, подключенный к http-серверу
 const io = require('socket.io')(server, {
   cors: {
     origin: '*'
@@ -6,28 +10,40 @@ const io = require('socket.io')(server, {
 })
 const log = console.log
 
+//Подключаем обработчики событий
 const registerMessageHandlers = require('./handlers/messageHandlers')
 const registerUserHandlers = require('./handlers/userHandlers')
 
+//Данная функция будет выполнятся при подключении каждого сокета
 const onConnection = (socket) => {
   log('User connected')
 
+  //Получаем название комнаты из строки запроса "рукопожатия"
   const { roomId } = socket.handshake.query
+  //Сохраняем название комнаты в соответствующем свойстве сокета
   socket.roomId = roomId
 
+  //Присоединяемся к комнате
   socket.join(roomId)
 
+  //Регистрируем обработчики
   registerMessageHandlers(io, socket)
   registerUserHandlers(io, socket)
 
+  //Обрабатываем отключение сокета-пользователя
   socket.on('disconnect', () => {
     log('User disconnected')
+    //Покидаем комнату
     socket.leave(roomId)
   })
 }
 
+//Обрабатываем подключение
+//Обработчик событий io.on обрабатывает события подключения, отключения и т.д.
+//на событие connection мы устанавливаем функцию обработчик, которая принимает объект сокета
 io.on('connection', onConnection)
 
+//Запускаем сервер
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
   console.log(`Server ready. Port: ${PORT}`)
